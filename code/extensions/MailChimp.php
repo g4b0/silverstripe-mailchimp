890d38c371fa7a619346aa785ac48dfaec8470fe
helpfulrobot <chris@silverstripe.com>
2016-01-01 04:34:12 +1300

    Converted to PSR-2

--- code/extensions/MailChimp.php
+++ code/extensions/MailChimp.php
@@ -1,15 +1,15 @@
 <?php
 
-class MailChimp extends DataExtension {
-	
-	/**
-	 * Recupera il Form dal controller
-	 * @return Form
-	 */
-	public function McSubscribeForm() {
-		$controller = new MailChimpController();
-		return $controller->McSubscribeForm();
-	}
-
+class MailChimp extends DataExtension
+{
+    
+    /**
+     * Recupera il Form dal controller
+     * @return Form
+     */
+    public function McSubscribeForm()
+    {
+        $controller = new MailChimpController();
+        return $controller->McSubscribeForm();
+    }
 }
-
